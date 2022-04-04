7ce5cb4cd8aae4ae623ea3a2946ebed738cbf711
steven <steven.john.ghobrial@gmail.com>
2022-04-04 20:30:39 +0200

    Final Change

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+
+// ReactDOM.createRoot(<App />, document.getElementById('root'));
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
